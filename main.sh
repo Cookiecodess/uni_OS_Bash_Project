ac79e09490a9b3e77242c09bd783d1141489fbe5
lynn <songlynn12@gmail.com>
2025-04-08 04:33:06 +0000

    validation ok

--- main.sh
+++ main.sh
@@ -1,22 +1,52 @@
 
 function addNew() {
+    continue="y"
+    while [[ "$continue" == "y" ]] do
+        read -p "Patron ID:" pID
+            while [[ -z "$pID" ]]; do
+                echo -e "Sorry the Patron ID cannot left blank."
+                read -p "Patron ID:" pID
+            done
+        read -p "First Name:" fname
+            while [[ -z "$fname" ]]; do
+                echo -e "Sorry the First Name cannot left blank."
+                read -p "First Name:" fname
+            done
+        read -p "Last Name:" lname
+            while [[ -z "$lname" ]]; do
+                echo -e "Sorry the Last Name cannot left blank."
+                read -p "Last Name:" lname
+            done
+        read -p "Mobile Number(01#-########):" phnum
+            while ! [[ "$phnum" =~ ^01[0-9]{1}-[0-9]{7,8}$ ]]; do
+                echo -e "Sorry the Phone Number Format is wrong."
+                read -p "Mobile Number:" phnum
+            done
+        read -p "Birth Date (MM-DD-YYYY):" bdate
+            while ! [[ "$bdate" =~ ^[0-1][0-9]-[0-3][0-9]-[0-9]{4}$ ]]; do
+                echo -e "Sorry the Birth Date Format is wrong."
+                read -p "Birth Date (MM-DD-YYYY):" bdate
+            done     
+        read -p "Membership type (Student / Public):" memberType
+            while [[ "$memberType" != "Student" && "$memberType" != "Public" ]]; do
 
-    read -p "Patron ID:" pID
-    read -p "First Name:" fname
-    read -p "Last Name:" lname
-    read -p "Mobile Number:" phnum
-    read -p "Birth Date (MM-DD-YYYY):" bdate
-    read -p "Membership type (Student / Public):" memberType
-    read -p "Joined Date (MM-DD-YYYY):" joinedDate
-    echo "Press (q) to return to Patron Maintenance Menu."
-    read -p "Add another new patron details? (y)es or (q)uit :" chooseAddNew
+                echo -e "Sorry Please Select (Student/Public)."
+                read -p "Membership type (Student / Public):" memberType
+            done
+        read -p "Joined Date (MM-DD-YYYY):" joinedDate
+            while ! [[ "$joinedDate" =~ ^[0-1][0-9]-[0-3][0-9]-[0-9]{4}$ ]]; do
+                echo -e "Sorry the Joined Date Format is wrong."
+                read -p "Joined Date (MM-DD-YYYY):" joinedDate
+            done 
 
-    echo -e "PatronID:FName:LName:MobileNum:BirthDate:Type:JoinedDate">>patron.txt
-    echo -e "$pID:$fname:$lname:$phnum:$bdate:$memberType:$joinedDate">>patron.txt
-    
-# P1000:Chai Lim:Chin:012-12345678:10-20-1970:Student:01-10-2025
-# P1003:Loh Mian:Simon:017-88888888:03-25-2020:Public:02-01-2025
-# P1001:Muhamad Abu Bakar:Bin Rashid:014-78945612: 11-29-1990:Student:01-02-2025
+        echo "Press (q) to return to Patron Maintenance Menu."
+        read -p "Add another new patron details? (y)es or (q)uit :" continue
+            #  if [ "$continue" == "q" ];
+            #     then echo "yes"
+            # fi
+        #echo -e "PatronID:FName:LName:MobileNum:BirthDate:Type:JoinedDate">>patron.txt
+        echo -e "$pID:$fname:$lname:$phnum:$bdate:$memberType:$joinedDate">>patron.txt
+    done
 
 }
 
