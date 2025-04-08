fb720b25d07f95812b0f0bc45673a3ece5c45b68
lynn <songlynn12@gmail.com>
2025-04-08 02:55:25 +0000

    good

--- main.sh
+++ main.sh
@@ -1,5 +1,26 @@
-#!/bin/bash
-read -p "What's your name? " name
-echo "Hello, $name!"
 
-echo "haha"
+echo "Patron Maintenance Menu"
+declare -a keys=("A" "S" "U" "D" "L" "P" "J" "Q")
+declare -A choices
+choices["A"]="Add New Patron Details"
+choices["S"]="Search a Patron (by Patron ID)"
+choices["U"]="Update a Patron Details"
+choices["D"]="Delete a Patron Details"
+choices["L"]="Sort Patrons by Last Name"
+choices["P"]="Sort Patrons by Patron ID"
+choices["J"]="Sort Patrons by Joined Date (Newest to Oldest Date)"
+choices["Q"]="Exit from Program"
+
+
+
+for key in "${keys[@]}"; do
+    if [$key=="Q"];
+        then echo "\n"
+    fi
+    echo "$key - ${choices[$key]}"
+    
+done
+
+read -p "Please select a choice:" key
+echo "Hello, ${choices[$key]}!"
+
